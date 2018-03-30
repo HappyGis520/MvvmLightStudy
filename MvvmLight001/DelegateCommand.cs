1b334feef7e6233493fd9d75127c8d9d97b3ccaa
Wangjianjun <595303122n@qq.com>
2018-03-30 17:00:24 +0800

    地

---
+++ MvvmLight001/DelegateCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MvvmLight001
+{
+    class DelegateCommand<T> : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (CanExecuteChanged == null)
+            {
+                throw new NotImplementedException();
+            }
+            else
+           {
+               CanExecuteChanged.BeginInvoke(this,<T>Parameter)
+               
+           }
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MvvmLight002/Model/WelcomeModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Streaming.Adaptive;
+using GalaSoft.MvvmLight;
+
+namespace MvvmLight002.Model
+{
+     public  class WelcomeModel:ObservableObject
+     {
+        private string introduction;
+
+        public string Introduction
+        {
+            get => introduction;
+            set
+            {
+                introduction = value; 
+                RaisePropertyChanged(() => Introduction);
+            }
+        }
+        
+     }
+
+    public class  wjj
+    {
+         public String abc;
+
+        public string Getname( [CallerMemberName]string abc="")
+        {
+            return abc+abc;
+
+        }
+
+    }
+}
